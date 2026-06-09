f64a2dc77ccd23ce10b84c26cef76fca5186e040
Defective <def3ctive4@gmail.com>
2026-06-09 21:14:48 +0200

    Rework CLI

--- src/main/java/io/github/defective4/audioanalyzer/CLI.java
+++ src/main/java/io/github/defective4/audioanalyzer/CLI.java
@@ -6,6 +6,7 @@ import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -32,36 +33,78 @@ import io.github.defective4.audioanalyzer.subsonic.model.Entity;
 
 public class CLI {
 
+    private interface CLIConsumer {
+        void consume(CommandLine cli, CLI prog) throws Exception;
+
+        String desc();
+
+        Options ops();
+    }
+
+    private static final Options ANALYSIS_OPTIONS;
+
+    private static final Map<String, CLIConsumer> COMMANDS = Map.of("analyze", new CLIConsumer() {
+
+        @Override
+        public void consume(CommandLine cli, CLI prog) throws Exception {
+            prog.index(!cli.hasOption('n'));
+        }
+
+        @Override
+        public String desc() {
+            return "(Re)analyze tracks from a remote Subsonic instance";
+        }
+
+        @Override
+        public Options ops() {
+            return ANALYSIS_OPTIONS;
+        }
+    }, "generate-playlist", new CLIConsumer() {
+        @Override
+        public void consume(CommandLine cli, CLI prog) throws Exception {
+            prog.groupTracks();
+        }
+
+        @Override
+        public String desc() {
+            return "Group tracks into playlists by mood, similarity, etc.";
+        }
+
+        @Override
+        public Options ops() {
+            return PLAYLIST_OPTIONS;
+        }
+
+    });
+    private static final Options COMMON_OPTIONS;
     private static final String DEFAULT_ESSENTIA = "http://127.0.0.1:8000/";
+
     private static final String DEFAULT_JDBC = "jdbc:sqlite:./mood.sqlite";
     private static final int DEFAULT_LIMIT = 30;
 
-    private static final Options options = new Options()
-            .addOption(Option.builder("h").desc("Display this help section").longOpt("help").build())
-            .addOption(Option.builder("a").desc("(re)analyze music database").longOpt("analyze").build())
-            .addOption(Option.builder("n").desc("Analyze all tracks, even if they are present in the database")
-                    .longOpt("only-new").build())
-            .addOption(Option.builder("j").desc("JDBC URL for the database (default " + DEFAULT_JDBC + ")")
-                    .longOpt("jdbc").numberOfArgs(1).argName("url").build())
-            .addOption(Option.builder("u").desc("Subsonic username (Required)").longOpt("user").numberOfArgs(1)
-                    .argName("username").required().build())
-            .addOption(Option.builder("p").desc("Subsonic password (Required)").longOpt("password").numberOfArgs(1)
-                    .argName("pass").required().build())
-            .addOption(Option.builder("s").desc("Subsonic instance URL (Required)").longOpt("url").numberOfArgs(1)
-                    .argName("url").required().build())
-            .addOption(Option.builder("t").desc("Essentia analyzer URL (Default " + DEFAULT_ESSENTIA + ")")
-                    .numberOfArgs(1).argName("url").build())
-
-            .addOption(Option.builder("g").desc("Generate a playlist").longOpt("playlist-generate").build());
-//            .addOption(Option.builder().longOpt("playlist-name").numberOfArgs(1).argName("name")
-//                    .desc("A name of playlist to generate").build())
-//            .addOption(Option.builder().longOpt("playlist-limit").numberOfArgs(1).argName("limit")
-//                    .desc("Limit of songs to be added to the playlist. (Default " + DEFAULT_LIMIT + ")").build())
-//            .addOption(Option.builder().longOpt("playlist-similar").numberOfArgs(1).argName("track ID or name")
-//                    .desc("Group tracks based on similarity to the provided track").build());
-
+    private static final Options PLAYLIST_OPTIONS;
+    static {
+        COMMON_OPTIONS = new Options()
+                .addOption(Option.builder("h").desc("Display this help section").longOpt("help").build())
+                .addOption(Option.builder("n").desc("Analyze all tracks, even if they are present in the database")
+                        .longOpt("only-new").build())
+                .addOption(Option.builder("j").desc("JDBC URL for the database (default " + DEFAULT_JDBC + ")")
+                        .longOpt("jdbc").numberOfArgs(1).argName("url").build())
+                .addOption(Option.builder("u").desc("Subsonic username (Required)").longOpt("user").numberOfArgs(1)
+                        .argName("username").required().build())
+                .addOption(Option.builder("p").desc("Subsonic password (Required)").longOpt("password").numberOfArgs(1)
+                        .argName("pass").required().build())
+                .addOption(Option.builder("s").desc("Subsonic instance URL (Required)").longOpt("url").numberOfArgs(1)
+                        .argName("url").required().build());
+        ANALYSIS_OPTIONS = new Options().addOptions(COMMON_OPTIONS)
+                .addOption(Option.builder("t").desc("Essentia analyzer URL (Default " + DEFAULT_ESSENTIA + ")")
+                        .numberOfArgs(1).argName("url").build());
+        PLAYLIST_OPTIONS = new Options().addOptions(COMMON_OPTIONS);
+    }
     private final String analyzerURL;
+
     private final SubsonicAPI api;
+
     private final Database db;
     private final Logger logger = LoggerFactory.getLogger(CLI.class);
 
@@ -165,35 +208,39 @@ public class CLI {
     }
 
     public static void main(String[] args) throws Exception {
+        if (args.length < 1 || !COMMANDS.containsKey(args[0])) {
+            System.err
+                    .println(!COMMANDS.containsKey(args[0]) ? "Invalid command " + args[0] : "Usage: <command> [args]");
+            System.err.println("Valid commands are:");
+            COMMANDS.forEach((k, v) -> { System.err.println(" - %s: %s".formatted(k, v.desc())); });
+            System.exit(1);
+            return;
+        }
+        Options options = COMMANDS.get(args[0]).ops();
         CommandLine cli;
         try {
-            cli = DefaultParser.builder().build().parse(options, args);
+            cli = DefaultParser.builder().build().parse(options, Arrays.copyOfRange(args, 1, args.length));
+            if (!cli.hasOption('h')) {
+                String jdbc = cli.hasOption('j') ? cli.getOptionValue('j') : DEFAULT_JDBC;
+                String user = cli.getOptionValue('u');
+                String password = cli.getOptionValue('p');
+                String subsonicURL = cli.getOptionValue('s');
+                if (!subsonicURL.endsWith("/")) subsonicURL = subsonicURL + "/";
+                String essentiaURL = cli.hasOption('t') ? cli.getOptionValue('t') : DEFAULT_ESSENTIA;
+                if (!essentiaURL.endsWith("/")) essentiaURL = essentiaURL + "/";
 
-            String jdbc = cli.hasOption('j') ? cli.getOptionValue('j') : DEFAULT_JDBC;
-            String user = cli.getOptionValue('u');
-            String password = cli.getOptionValue('p');
-            String subsonicURL = cli.getOptionValue('s');
-            if (!subsonicURL.endsWith("/")) subsonicURL = subsonicURL + "/";
-            String essentiaURL = cli.hasOption('t') ? cli.getOptionValue('t') : DEFAULT_ESSENTIA;
-            if (!essentiaURL.endsWith("/")) essentiaURL = essentiaURL + "/";
-
-            CLI prog = new CLI(jdbc, user, password.toCharArray(), subsonicURL, essentiaURL);
-            if (cli.hasOption('a')) {
-                prog.index(!cli.hasOption('n'));
-                return;
-            } else if (cli.hasOption('g')) {
-                prog.groupTracks();
+                CLI prog = new CLI(jdbc, user, password.toCharArray(), subsonicURL, essentiaURL);
+                COMMANDS.get(args[0]).consume(cli, prog);
                 return;
             }
-            System.err.println("One of --analyze, or --playlist-generate is required");
         } catch (MissingArgumentException | MissingOptionException e) {
             System.err.println(e.getMessage());
         }
 
-        new HelpFormatter().printHelp(
-                Path.of(CLI.class.getProtectionDomain().getCodeSource().getLocation().getFile()).getFileName()
-                        + " [options]",
-                options);
+        new HelpFormatter().printHelp("%s %s [options]".formatted(
+                Path.of(CLI.class.getProtectionDomain().getCodeSource().getLocation().getFile()).getFileName(),
+                args[0]), options);
+        System.exit(2);
     }
 
     private static double calculateSimilarity(Track track1, Track track2) {
