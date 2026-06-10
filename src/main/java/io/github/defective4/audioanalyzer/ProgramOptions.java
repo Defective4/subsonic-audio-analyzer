d6b2c5416d1332e267337919bfc353fb1301a3c3
Defective <def3ctive4@gmail.com>
2026-06-11 00:03:33 +0200

    Add dots to all options' descriptions

--- src/main/java/io/github/defective4/audioanalyzer/ProgramOptions.java
+++ src/main/java/io/github/defective4/audioanalyzer/ProgramOptions.java
@@ -36,29 +36,29 @@ public class ProgramOptions {
 
         AN_TENSORFLOW = Option.builder("t").desc("Essentia analyzer URL (Default " + DEFAULT_ESSENTIA + ")")
                 .numberOfArgs(1).argName("url").build();
-        AN_ALL = Option.builder("a").desc("Analyze all tracks, even if they are present in the database").longOpt("all")
-                .build();
+        AN_ALL = Option.builder("a").desc("Analyze all tracks, even if they are present in the database.")
+                .longOpt("all").build();
 
         PLS_GENRE_FILTER_OPTION = Option.builder().longOpt("genre-filter").numberOfArgs(1).argName("genre")
-                .desc("Filter songs based on their genre. Pass ?list to list available genres").build();
+                .desc("Filter songs based on their genre. Pass ?list to list available genres.").build();
         PLS_INSTRUMENT_FILTER_OPTION = Option.builder().longOpt("instrument-filter").numberOfArgs(1)
                 .argName("instrument")
-                .desc("Filter songs based on their primary instrument. Pass ?list to list available instruments")
+                .desc("Filter songs based on their primary instrument. Pass ?list to list available instruments.")
                 .build();
         PLS_LIMIT_OPTION = Option.builder().argName("n").numberOfArgs(1).longOpt("limit")
                 .desc("Limit the playlists to n tracks. (Default " + DEFAULT_LIMIT + ")").converter(Integer::parseInt)
                 .build();
         PLS_MOOD_FILTER_OPTION = Option.builder().longOpt("mood-filter").numberOfArgs(1).argName("mood")
-                .desc("Filter songs based on their mood. Pass ?list to list available moods").build();
+                .desc("Filter songs based on their mood. Pass ?list to list available moods.").build();
         PLS_NAME_OPTION = Option.builder().longOpt("playlist-name").numberOfArgs(1).argName("name")
-                .desc("Human readable name of the new playlist. Required if --replace-playlist is not used").build();
-        PLS_PUBLIC_OPTION = Option.builder().longOpt("public").desc("If present, the NEW playlist will be public")
+                .desc("Human readable name of the new playlist. Required if --replace-playlist is not used.").build();
+        PLS_PUBLIC_OPTION = Option.builder().longOpt("public").desc("If present, the NEW playlist will be public.")
                 .build();
         PLS_REPLACE_OPTION = Option.builder().longOpt("replace-playlist").numberOfArgs(1).argName("id").desc(
                 "Replace an existing playlist instead of creating a new one. This will remove all songs from an existing playlist in favor of new ones.")
                 .build();
         PLS_SIMILAR_SONG_OPTION = Option.builder().longOpt("similar-song").numberOfArgs(1).argName("song")
-                .desc("ID or name of a base song to find similar songs to it").build();
+                .desc("ID or name of a base song to find similar songs to it.").build();
     }
 
     private ProgramOptions() {}
